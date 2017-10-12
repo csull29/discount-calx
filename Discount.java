c672d9407f2dd18c6708ee64a1a940686ebd31f6
Craig Sullivan <craigsullivan29@gmail.com>
2017-10-12 17:39:15 +0100

    Add files via upload

---
+++ Discount.java
@@ -0,0 +1,95 @@
+/*
+* @author Craig Sullivan
+* 11/05/2017
+* May 2017 Exam
+* Discount.java
+*/
+
+//Name of class
+public class Discount{
+
+//Declaration of data members/instance variables
+	private String member;
+	private String time;
+	private int cost;
+	private double discount;
+
+//Constructor method - No parameters!
+	public Discount(){
+	}
+
+//Declaring setter methods
+	public void setMember(String member){
+		this.member=member;
+	}
+
+	public void setTime(String time){
+		this.time=time;
+	}
+
+	public void setCost(int cost){
+		this.cost=cost;
+	}
+
+//Compute method - to determine which discount should be applied to the customers shopping
+	public void compute(){
+
+	if(member.equals("yes")){
+		if(time.equals("morning")){
+			if(cost<=100){
+				discount=(0.025*cost);
+			} else if(cost>100){
+				discount=(0.05*cost);
+			}
+		}
+	}
+
+	if(member.equals("yes")){
+		if(time.equals("evening")){
+			if(cost<=100){
+				discount=(0.05*cost);
+			} else if(cost>100){
+				discount=(0.1*cost);
+			}
+		}
+	}
+
+	if(member.equals("no")){
+		if(time.equals("morning")){
+			if(cost<=100){
+				discount=(0*cost);
+			} else if(cost>100){
+				discount=(0*cost);
+			}
+		}
+	}
+
+	if(member.equals("no")){
+		if(time.equals("evening")){
+			if(cost<=100){
+				discount=(0.02*cost);
+			} else if(cost>100){
+				discount=(0.04*cost);
+			}
+		}
+	}
+
+
+
+}
+
+//Getter method
+	public double getDiscount(){
+		return discount;
+}
+
+
+
+
+
+
+
+
+
+
+} //Ends class
---
+++ DiscountApp.java
@@ -0,0 +1,61 @@
+/*
+* @author Craig Sullivan
+* 11/05/2017
+* May 2017 Exam
+* DiscountApp.java
+*/
+
+import javax.swing.JOptionPane; //Imports GUI input/output window
+
+//Class Name
+public class DiscountApp{
+
+//Main method
+	public static void main (String args[]){
+
+//Users Inputs
+		String member="";
+		String time="";
+		int cost=0;
+		double discount=0.0;
+
+//Declare Objects
+	Discount myDiscount=new Discount();
+
+
+//Input - Welcome
+	JOptionPane.showMessageDialog(null, "Welcome to your local Market. Please enter iwhether or not, you are a member; followed by the time of day; and finally the total cost of your shopping. We will then calculate your discount!");
+
+//Asking of the questions and storing answers
+
+	member=JOptionPane.showInputDialog(null, "Are you a Market member? Please answer 'Yes' or 'No'.");
+//Sends to instan class
+		myDiscount.setMember(member);
+
+	time=JOptionPane.showInputDialog(null, "What time of day is it? Please answer 'Morning' or 'Evening'.");
+	myDiscount.setTime(time);
+
+	cost=Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter the total cost of your shopping."));
+	myDiscount.setCost(cost);
+
+//Compute the process
+	myDiscount.compute();
+
+//Output
+	discount=myDiscount.getDiscount();
+	JOptionPane.showMessageDialog(null, " Your total discount for the cost of your shopping is " +discount);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
